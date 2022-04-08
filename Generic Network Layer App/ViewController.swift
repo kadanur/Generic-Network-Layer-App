518d2c663e76a1b279249bed8525b333e4dab0ea
kadanur <mkadanur@gmail.com>
2022-04-08 13:48:24 +0300

    Initial Commit

---
+++ Generic Network Layer App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Generic Network Layer App
+//
+//  Created by M Kaan Adanur on 8.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
